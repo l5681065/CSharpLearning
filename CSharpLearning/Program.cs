96e60fde665e5c167d84f7a5b0ddb7616e78ee6d
l5681065 <greatxueyou@163.com>
2024-12-12 19:05:03 +0800

    C#Learning

---
+++ CSharpLearning/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CSharpLearning
+{
+    internal class Program
+    {
+        static IEnumerable<int> ProduceEvenNumbers(int upto)
+        {
+            for (int i = 0; i <= upto; i += 2)
+            {
+                yield return i;
+            }
+        }
+
+        static IEnumerable<int> GetNumbers()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (int i in ProduceEvenNumbers(9))
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+            // Output: 0 2 4 6 8
+
+            foreach (int number in GetNumbers())
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
